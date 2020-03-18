947fbe798dfa54669bed9a29c16cbc4de2fb9604
Magno <magnoneves2@gmail.com>
2020-03-18 20:14:09 -0300

    Desenvolvimento do botão de doação

---
+++ botao-doar/javascript.js
@@ -0,0 +1,33 @@
+
+let escolha1 = document.getElementById('radio-179');
+let escolha2 = document.getElementById('radio-279').value;
+let escolha3 = document.getElementById('radio-379').value;
+let escolha4 = document.getElementById('radio-479').value;
+let escolha5 = document.getElementById('radio-579').value;
+
+
+document.getElementById("btnEnviar").addEventListener('click', () => {
+    let resultado = "";
+    if (resultado === escolha1.value) {
+        return resultado;
+    };
+
+    // if (resultado = escolha2) {
+    //     return resultado;
+    // };
+
+    // if (resultado = escolha3) {
+    //     return resultado;
+    // };
+
+    // if (resultado = escolha4) {
+    //     return resultado;
+    // };
+
+    // if (resultado = escolha4) {
+    //     return resultado;
+    // };
+
+    console.log(resultado);
+});
+
